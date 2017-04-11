f6de885b20cf8491c0551bc4cd3cd8e0c2ae1176
umeshprasad123 <umeshprasad@gmail.com>
2017-04-10 19:05:56 -0700

    Add files via upload
    Enabling discovery of locations.csv via classpth

--- jersey-service/src/main/java/com/kinsa/rest/data/ProjectProperty.java
+++ jersey-service/src/main/java/com/kinsa/rest/data/ProjectProperty.java
@@ -2,7 +2,10 @@ package com.kinsa.rest.data;
 
 // TODO : Read project properties from a file.
 public class ProjectProperty {
-    public static String geocodingKey = "AIzaSyBYen5eFqxsGAG274HtJzD0DgLwE0IFfFo";
-    public static String dataFile = "C:/ucp/misc/kinsa/jersey-service/locations.csv" ;
+    public static String geocodingKey   = "AIzaSyBYen5eFqxsGAG274HtJzD0DgLwE0IFfFo";
+    public static String dataFile       = "locations.csv" ;
+
+    // Fall back if dataFile is not found in the classpath
+    public static String absPathDtaFile = "C:/ucp/misc/examples/jsontest/jersey-service/src/main/resources/locations.csv" ;
 
 }
