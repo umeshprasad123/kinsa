3bd0ac79eabc32890635e1b556baeafc709677b9
umeshprasad123 <umeshprasad@gmail.com>
2017-04-10 19:09:03 -0700

    Add files via upload
    Enabling discovery of locations.csv via classpth

--- jersey-service/src/main/java/com/kinsa/rest/data/CoffeeShopList.java
+++ jersey-service/src/main/java/com/kinsa/rest/data/CoffeeShopList.java
@@ -3,8 +3,11 @@ package com.kinsa.rest.data;
 import com.kinsa.rest.model.CoffeeShop;
 import com.kinsa.rest.util.Helper;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -16,13 +19,30 @@ public class CoffeeShopList {
 
     private static CopyOnWriteArrayList<CoffeeShop> csList = new CopyOnWriteArrayList<>();
 
-    private static Properties prop = new Properties();
-    private static String dataFile =  ProjectProperty.dataFile ; // "locations.csv" ; // Default data file in JSON format.
+    private static Properties prop          = new Properties();
+    private static String dataFilePath      =  ProjectProperty.dataFile ; // "locations.csv" ; // Default data file in JSON format.
+    private static String dataFileAbsPath   = ProjectProperty.absPathDtaFile;
 
     private CoffeeShopList() {}
 
     static {
         List<CoffeeShop> csList = CoffeeShopList.getInstance();
+        // System.out.println( dataFilePath);
+
+        File dataFile ;
+        URL path = ClassLoader.getSystemResource(dataFilePath);
+        if ( path == null ) {
+            dataFile = new File(dataFileAbsPath);
+            System.out.println(" URI Path is null ... ");
+        }else {
+            try {
+                dataFile = new File(path.toURI());
+            }catch(URISyntaxException uris) {
+                // Fall back to absolute file.
+                dataFile = new File(dataFileAbsPath);
+            }
+        }
+
         Helper.readCoffeeShopLocation(csList, dataFile);
     }
 
